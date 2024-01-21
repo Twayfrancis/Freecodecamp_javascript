be9578bea706256a6d7524277a60e99e92397f40
Twayfrancis <tawaifrancis@gmail.com>
2024-01-21 17:35:58 +0300

    button1 is a variable that is not going to be reassigned. If you are not going to assign a new value to a variable, it is best practice to use the const keyword to declare it instead of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it.Change your button1 variable to be declared with the const keyword.

--- role_playing_game/script.js
+++ role_playing_game/script.js
@@ -6,4 +6,4 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick", "dagger", "sword"];
 
-let button1 = document.querySelector("#button1");
+const button1 = document.querySelector("#button1");
