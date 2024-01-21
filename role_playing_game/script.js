d6053f3d8be493cd7dffa9efccf84a87fee2c95e
Twayfrancis <tawaifrancis@gmail.com>
2024-01-21 22:34:07 +0300

    Add a second object to your locations array (remember to separate them with a comma). Following the pattern you used in the first object, create the same properties but use the values from the goStore function. Set the name property to store.~

--- role_playing_game/script.js
+++ role_playing_game/script.js
@@ -25,12 +25,20 @@ const monsterHealthText = document.querySelector("#monsterHealth");
  * this array stores objects
  * objects are indicated by curly braces
  */
-const locations = [{
+const locations = [
+  {
   name: "town square",
   "button text": ["Go to store", "Go to cave", "Fight dragon"],
   "button functions": [goStore, goCave, fightDragon],
   text: "You are in the town square. You see a sign that says \"Store\"."
-}];
+  },
+  {
+  name: "store",
+  "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+  "button functions": [buyHealth, buyWeapon, goTown],
+  text: "You enter the store."
+  }
+];
 /**
  * functions allow to run sections of code at specific times
  */
